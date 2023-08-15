1c40f843f8c2f255f03bb558326df00942060e35
nikolaus@LinuxMint22 <nikolaus@superbase.com>
2023-08-15 11:51:31 +0100

    Update tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [HomeComponent],
       declarations: [AppComponent],
     }),
   );
@@ -16,18 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nikmaxott-portfolio'`, () => {
+  it('should have a navigation bar with 3 links', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nikmaxott-portfolio');
   });
 
-  it('should render title', () => {
+  it('should have the dark mode toggle default', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'nikmaxott-portfolio app is running!',
-    );
+    const app = fixture.componentInstance;
+  });
+
+  it('should be able to toggle dark mode', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
   });
 });
